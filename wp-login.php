e8ab08a478f6337700bed0cf86ddc3d1cbfe4543
eviljay <evgencherkashyn@gmail.com>
2021-06-30 11:03:04 +0300

    11

--- wp-login.php
+++ wp-login.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
 	<!--[if IE 8]>
-		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-CA">
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US" xmlns:fb="http://ogp.me/ns/fb#" xmlns:addthis="http://www.addthis.com/help/api-spec" >
 	<![endif]-->
 	<!--[if !(IE 8) ]><!-->
-		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-CA">
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xmlns:fb="http://ogp.me/ns/fb#" xmlns:addthis="http://www.addthis.com/help/api-spec" >
 	<!--<![endif]-->
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Free Spins Slots &lsaquo; Log In</title>
-	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='http://www.freespinsslots.net/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6.1' type='text/css' media='all' />
+	<title> &rsaquo; Log In</title>
+	<link rel='stylesheet' id='buttons-css'  href='http://www.freespinsslots.net/wp-includes/css/buttons.min.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://www.freespinsslots.net/wp-includes/css/dashicons.min.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://www.freespinsslots.net/wp-admin/css/login.min.css?ver=4.0' type='text/css' media='all' />
+<script type='text/javascript' src='http://www.freespinsslots.net/wp-includes/js/jquery/jquery.js?ver=1.11.1'></script>
+<script type='text/javascript' src='http://www.freespinsslots.net/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var strong_password_error_text = {"text":"Sorry, but you must enter a strong password."};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://www.freespinsslots.net/wp-content/plugins/better-wp-security/modules/free/strong-passwords/js/strong-passwords.js?ver=4032'></script>
 <meta name='robots' content='noindex,follow' />
 	</head>
-	<body class="login login-action-login wp-core-ui  locale-en-ca">
-		<div id="login">
-		<h1><a href="https://en-ca.wordpress.org/" title="Powered by WordPress" tabindex="-1">Free Spins Slots</a></h1>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+	<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1"></a></h1>
 	
 <form name="loginform" id="loginform" action="http://www.freespinsslots.net/wp-login.php" method="post">
 	<p>
-		<label for="user_login">Username or Email<br />
+		<label for="user_login">Username<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
 	</p>
 	<p>
@@ -34,7 +44,7 @@
 </form>
 
 <p id="nav">
-	<a href="http://www.freespinsslots.net/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="http://www.freespinsslots.net/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -51,12 +61,18 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://www.freespinsslots.net/">&larr; Back to Free Spins Slots</a></p>
+	<p id="backtoblog"><a href="http://www.freespinsslots.net/" title="Are you lost?">&larr; Back to </a></p>
 	
 	</div>
 
 	
-		<div class="clear"></div>
+	<link rel='stylesheet' id='output-css'  href='http://www.freespinsslots.net/wp-content/plugins/addthis/css/output.css?ver=4.0' type='text/css' media='all' />
+	<div class="clear"></div>
 	</body>
 	</html>
 	
+<!-- Performance optimized by W3 Total Cache. Learn more: http://www.w3-edge.com/wordpress-plugins/
+
+Page Caching using disk: enhanced (Requested URI is rejected)
+
+ Served from: www.freespinsslots.net @ 2014-09-23 11:42:03 by W3 Total Cache -->
